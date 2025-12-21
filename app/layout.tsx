ad45d5f4b147e34f37a252340bb06090b4579c4f
armandwipangestu <armandwi.pangestu7@gmail.com>
2025-12-21 17:59:50 +0700

    feat: add site header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { SiteHeader } from "@/components/site-header";
 
 const geistSans = Geist({
   variable: "--font-sans",
@@ -28,7 +29,12 @@ export default function RootLayout({
       <body
         className={cn("min-h-screen bg-background font-sans antialiased", `${geistSans.variable} ${geistMono.variable}`)}
       >
-        {children}
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <SiteHeader />
+          <main className="flex-1">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
---
+++ components/site-header.tsx
@@ -0,0 +1,35 @@
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { Icons } from "./icons";
+import { MainNav } from "./main-nav";
+
+export function SiteHeader() {
+    return <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 max-4-screen-2xl items-center">
+            <MainNav />
+            <div className="flex flex-1 items-center justify-end space-x-2">
+                <nav className="flex items-center">
+                    <Link href={siteConfig.links.github} target="_blank" rel="norefferrer noopener">
+                        <div className={cn(buttonVariants({
+                            variant: "ghost",
+                        }), "w-10 px-0")}>
+                            <Icons.gitHub className="h-4 w-4" />
+                            <div className="sr-only">GitHub</div>
+                        </div>
+                    </Link>
+
+                    <Link href={siteConfig.links.instagram} target="_blank" rel="norefferrer noopener">
+                        <div className={cn(buttonVariants({
+                            variant: "ghost",
+                        }), "w-10 px-0")}>
+                            <Icons.isntagram className="h-4 w-4" />
+                            <div className="sr-only">Instagram</div>
+                        </div>
+                    </Link>
+                </nav>
+            </div>
+        </div>
+    </header>
+}
