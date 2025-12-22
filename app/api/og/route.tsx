773eeae50a209e2f6fcebb93feebf02247831444
armandwipangestu <armandwi.pangestu7@gmail.com>
2025-12-22 07:53:24 +0700

    fix: remove un-used variable

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -69,7 +69,7 @@ export async function GET(req: NextRequest) {
             }]
         })
 
-    } catch (error) {
+    } catch {
         return new Response("Failed to generate image", {
             status: 500
         })
