edc4cde61002ac4ea7d5b1ba4e308a72ec81746e
armandwipangestu <armandwi.pangestu7@gmail.com>
2025-12-22 18:19:32 +0700

    fix(ci): build velite

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const isDev = process.argv.indexOf('dev') !== -1
 const isBuild = process.argv.indexOf('build') !== -1
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+const isCI = process.env.CI === 'true'
+
+if (!isCI && !process.env.VELITE_STARTED && (isDev || isBuild)) {
   process.env.VELITE_STARTED = '1'
   import('velite').then(m => m.build({ watch: isDev, clean: !isDev }))
 }
